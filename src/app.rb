49eeca4c7a6ef0787452c03f89a91137a2f418f8
tspider0176 <1a38d3a10f5e8.16h@gmail.com>
2016-11-24 02:00:26 +0900

    Add some samples

--- src/app.rb
+++ src/app.rb
@@ -6,8 +6,13 @@ class MainApp < Sinatra::Base
   configure :development do
     register Sinatra::Reloader
   end
+
+  configure do
+    set :stat, {}
+  end
+
   get '/' do
-    'hello, world'
+    "hello, world"
   end
 
   get '/now' do
@@ -24,7 +29,47 @@ class MainApp < Sinatra::Base
 
   get '/file/:name' do
     # :name で与えられたファイルを開き、中の内容を返すようなコード
-    # ファイルが存在しない場合は、ステータスコードを変え　エラーを返す。
+    # ファイルが存在しない場合は、ステータスコードを変えてエラーを返す。
+  end
+
+  post '/post', provides: :json do
+  params = JSON.parse(request.body.read, {:symbolize_names => true})
+    "#{params[:first].chars.first}.#{params[:last].chars.first}.\n"
+  end
+
+
+  put '/put' do
+    # 何らかの作成/更新処理
+    "#{request.body.read}"
+  end
+
+  put '/put_del' do
+    settings.stat = JSON.parse(request.body.read, {:symbolize_names => true})
+    status 204
+  end
+
+  delete '/put_del/:num' do
+    sym = params[:num].to_sym
+    if settings.stat[sym] != nil then
+      settings.stat[sym] = false
+      status 204
+    else
+      status 501
+    end
+  end
+
+  get '/put_del/:num' do
+    sym = params[:num].to_sym
+    if settings.stat[sym] == true then
+      "#{params[:num]}"
+    else
+      status 404
+    end
+  end
+
+  delete '/delete' do
+    # 何らかの削除処理
+    status 204
   end
 
   post '/file/:name/:content' do
@@ -39,7 +84,7 @@ class MainApp < Sinatra::Base
     # :name で与えられたファイルを削除するようなコード
   end
 
-  # JSONのやり取りをするコード ↓ 
+  # JSONのやり取りをするコード ↓
   # get
   # post
   # put
