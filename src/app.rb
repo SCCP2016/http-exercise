a0928d0a8f516db03fc0431179a38a98a3e35531
tspider0176 <1a38d3a10f5e8.16h@gmail.com>
2016-12-08 02:42:50 +0900

    Modify app

--- src/app.rb
+++ src/app.rb
@@ -32,44 +32,20 @@ class MainApp < Sinatra::Base
     # ファイルが存在しない場合は、ステータスコードを変えてエラーを返す。
   end
 
-  post '/post', provides: :json do
+  post '/', provides: :json do
   params = JSON.parse(request.body.read, {:symbolize_names => true})
     "#{params[:first].chars.first}.#{params[:last].chars.first}.\n"
   end
 
 
-  put '/put' do
+  put '/' do
     # 何らかの作成/更新処理
     "#{request.body.read}"
   end
 
-  put '/put_del' do
-    settings.stat = JSON.parse(request.body.read, {:symbolize_names => true})
+  delete '/' do
     status 204
-  end
-
-  delete '/put_del/:num' do
-    sym = params[:num].to_sym
-    if settings.stat[sym] != nil then
-      settings.stat[sym] = false
-      status 204
-    else
-      status 501
-    end
-  end
-
-  get '/put_del/:num' do
-    sym = params[:num].to_sym
-    if settings.stat[sym] == true then
-      "#{params[:num]}"
-    else
-      status 404
-    end
-  end
-
-  delete '/delete' do
     # 何らかの削除処理
-    status 204
   end
 
   post '/file/:name/:content' do
